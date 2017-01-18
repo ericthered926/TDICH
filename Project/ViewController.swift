0e37adc36beed8df84bca129d587027d929a6ff3
binfird <ericlarsen@horsebeef.app.byu.edu>
2017-01-18 12:46:55 -0700

    Initial Commit

---
+++ Project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project
+//
+//  Created by Eric Larsen on 1/18/17.
+//  Copyright © 2017 N00B$. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
